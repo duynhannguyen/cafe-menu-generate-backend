c16709a4018b39ec1f729118a9e8beb3b5b39b90
duylinh13 <huynhleduylinh1311@gmail.com>
2023-10-14 22:17:49 +0700

    update signup

--- controller/authController.js
+++ controller/authController.js
@@ -3,3 +3,40 @@ const login = () => {};
 export const AuthController = {
   login,
 };
+
+const signup = asyncHandler(async (req, res) => {
+  const { email, password } = req.body || {};
+
+  const existingUser = await db.users.findOne({ email });
+  if (!existingUser) {
+    res.status(400);
+    throw new Error("Invalid credentials");
+  }
+
+  const isMatchedPassword = await bcrypt.compare(
+    password,
+    existingUser.password
+  );
+
+  if (!isMatchedPassword) {
+    res.status(400);
+    throw new Error("Email or password is not valid");
+  }
+
+  const payload = {
+    id: existingUser._id,
+    email: existingUser.email,
+    fullname: existingUser.fullname,
+  };
+
+  const SECRET_KEY = process.env.SECRET_KEY;
+
+  const token = jwt.sign(payload, SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRES_TIME,
+  });
+
+  res.json({
+    message: "Signup successfully",
+    accessToken: token,
+  });
+});
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -3,3 +3,9 @@ import AuthController from "../controller/authController.js";
 const router = express.Router();
 
 router.post("/login", AuthController.login);
+
+router.post(
+  "/signup",
+  validateMdw(AuthValidator.registerSchema),
+  AuthController.register
+);
