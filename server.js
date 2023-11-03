dd8ede662c8435d200abd4ddae6898547ef1a6b9
HungTrinhIT <minhhung.it.work@gmail.com>
2023-11-03 17:21:31 +0700

    add health check

--- server.js
+++ server.js
@@ -1,34 +1,40 @@
-import express from "express";
-import "dotenv/config";
-import { connectToDatabase } from "./config/database.js";
-import handleErrorMdw from "./middlewares/handleError.mdw.js";
-import cors from "cors";
-import appRouter from "./routes/index.js";
+import express from 'express';
+import 'dotenv/config';
+import { connectToDatabase } from './config/database.js';
+import handleErrorMdw from './middlewares/handleError.mdw.js';
+import cors from 'cors';
+import appRouter from './routes/index.js';
 
-const whiteList = ["http://localhost:3001"];
+const whiteList = ['http://localhost:3001'];
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (process.env.NODE_ENV === "development") {
+    if (process.env.NODE_ENV === 'development') {
       return callback(null, true);
     }
     if (whiteList.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error("Not allowed by CORS"));
+      callback(new Error('Not allowed by CORS'));
     }
   },
-  methods: "PUT,PATCH,GET,DELETE,UPDATE",
+  methods: 'PUT,PATCH,GET,DELETE,UPDATE',
 };
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-app.use(cors(corsOptions));
-app.use("/api/v1", appRouter);
-app.use(handleErrorMdw);
+app.use(cors('*'));
+app.use('/api/v1', appRouter);
 
+app.get('/health', (req, res) => {
+  res.json({
+    status: 'oke',
+  });
+});
+
+app.use(handleErrorMdw);
 
 connectToDatabase();
 
